2a5e6985fae34e676df5a7d2353f68d21f17e133
raineworks <mail@raineworks.com>
2024-04-16 07:14:52 +0000

    feat: added tailwind and shadcnui

--- apps/gateway/src/routes/+page.svelte
+++ apps/gateway/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from '$lib/components/ui/button' 
+
+    const fetchGreeting = async () => {
+        const res = await fetch('/api/hooks')
+        const data = await res.json()
+        returnValue = data.message
+    }
+
+    $: returnValue = ''
+</script>
+
+<div>
+    <Button on:click={fetchGreeting}>Click Me!</Button>
+    <p>{returnValue}</p>
+</div>
