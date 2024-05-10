c5346ab698431dc827bf9d9c5110e6e0ebf277f9
raineworks <mail@raineworks.com>
2024-05-10 00:28:18 +0000

    added socket support

--- apps/gateway/src/index.ts
+++ apps/gateway/src/index.ts
@@ -1,8 +1,25 @@
+import { Hono } from 'hono';
 import { createBunWebSocket } from 'hono/bun';
-import { app } from '@src/app';
+import { drizzle } from '@middleware/db.middleware';
 import { env } from '@utils/env.util';
 
-const { websocket } = createBunWebSocket();
+const app = new Hono();
+const { websocket, upgradeWebSocket } = createBunWebSocket();
+
+app.use(drizzle);
+
+app.get('/', (c) => {
+	const userAgent = c.req.header('User-Agent');
+	console.log(userAgent);
+	return c.text('Hello World');
+});
+
+app.get(
+	'/ws',
+	upgradeWebSocket((c) => {
+		return {};
+	}),
+);
 
 const server = Bun.serve({
 	port: env.PORT,
